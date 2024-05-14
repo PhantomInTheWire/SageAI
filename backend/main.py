a0dc54440811bfad2bb997d74f0a4d2467afa92e
Karan <karanlokchandani@protonmail.com>
2024-05-15 00:20:05 +0530

    svg rendering

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from fastapi.responses import FileResponse
 
 from apps.roadmap import generate_svg
@@ -17,6 +17,8 @@ async def say_hello(name: str):
 
 
 @app.get("/code/{prompt}")
-async def get_svg(prompt: str):
+async def get_svg(prompt: str, response: Response):
     generate_svg(prompt)
-    return FileResponse('apps/file.svg', media_type="image/svg+xml")
+    response.headers["Content-Disposition"] = f"attachment; filename=backend/apps/file.svg"
+    response.headers["Content-Type"] = "image/svg+xml"
+    return FileResponse('backend/apps/file.svg', media_type="image/svg+xml")
